2cb1708d9a828acb6487d96cf0181abc28826eba
kapitulin24 <kapitulin24@gmail.com>
2026-02-18 23:55:55 +0300

    feat: Next.js, ESLint flat config, shadcn

---
+++ apps/frontend/src/pages/main/ui/MainPage.tsx
@@ -0,0 +1,44 @@
+import { FC } from 'react';
+import { Button } from 'shared/components/ui/button';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "shared/components/ui/alert-dialog"
+
+interface MainPageProps {
+  className?: string;
+}
+
+const MainPage: FC<MainPageProps> = ({className}) => {
+  return (
+    <div className={className}>
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button variant="default">Click me!</Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </div>
+  );
+};
+
+export default MainPage;
