3e896d67759e4321a65644208901379ba951080b
kapitulin24 <kapitulin24@gmail.com>
2026-02-21 11:50:04 +0300

    feat: Добавлены зависимости tanstack query и линтер для него

--- apps/frontend/eslint.config.mjs
+++ apps/frontend/eslint.config.mjs
@@ -2,11 +2,13 @@ import baseConfig from '../../eslint.config.mjs';
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import pluginQuery from '@tanstack/eslint-plugin-query'
 
 const eslintConfig = defineConfig([
   ...baseConfig,
   ...nextVitals,
   ...nextTs,
+  ...pluginQuery.configs['flat/recommended'],
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
